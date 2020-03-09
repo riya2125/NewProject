99c3d26aad421c5477e2258652329e3bf9706fb9
riya2125 <37058205+riya2125@users.noreply.github.com>
2020-03-09 22:40:34 +0530

    Add files via upload

---
+++ FoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.usc.foodordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class FoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingApplication.class, args);
+	}
+}
